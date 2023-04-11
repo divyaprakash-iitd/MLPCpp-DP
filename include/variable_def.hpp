b723a9c06694c52f9e392116436c41bbe8e17244
EvertBunschoten <evertbun@xs4all.nl>
2023-04-11 09:05:56 +0200

    Updated custom type def

--- include/variable_def.hpp
+++ include/variable_def.hpp
@@ -1,20 +1,5 @@
-#if defined(CODI_REVERSE_TYPE)  // reverse mode AD
-#include "codi.hpp"
-#include "codi/tools/data/externalFunctionUserData.hpp"
-
-#if defined(HAVE_OMP)
-using mlpdouble = codi::RealReverseIndexOpenMP;
+#ifdef MLP_CUSTOM_TYPE
+using mlpdouble = MLP_CUSTOM_TYPE;
 #else
-#if defined(CODI_INDEX_TAPE)
-using mlpdouble = codi::RealReverseIndex;
-#else
-using mlpdouble = codi::RealReverse;
-#endif
-#endif
-#elif defined(CODI_FORWARD_TYPE)  // forward mode AD
-#include "codi.hpp"
-using mlpdouble = codi::RealForward;
-
-#else  // primal / direct / no AD
 using mlpdouble = double;
 #endif
